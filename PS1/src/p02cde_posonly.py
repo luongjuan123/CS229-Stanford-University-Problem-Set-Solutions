91e63e1d22baa2a0aba79997ed40a8b49aa4d3ec
luongjuan123 <dungpubgame@gmail.com>
2025-09-29 11:19:23 +0700

    P02, P03 solved

--- PS1/src/p02cde_posonly.py
+++ PS1/src/p02cde_posonly.py
@@ -27,9 +27,40 @@ def main(train_path, valid_path, test_path, pred_path):
 
     # *** START CODE HERE ***
     # Part (c): Train and test on true labels
+    x_train, t_train = util.load_dataset(train_path, label_col='t', add_intercept=True)
+    x_test, t_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
+
+    model_t = LogisticRegression()
+    model_t.fit(x_train, t_train)
+
+    util.plot(x_test, t_test, model_t.theta, 'output/p02c.png')
+
+    t_pred_c = model_t.predict(x_test)
+    np.savetxt(pred_path_c, t_pred_c>0.5, fmt='%d')
     # Make sure to save outputs to pred_path_c
     # Part (d): Train on y-labels and test on true labels
+    x_train, y_train = util.load_dataset(train_path, label_col='y', add_intercept=True)
+    x_test, y_test = util.load_dataset(test_path, label_col='y', add_intercept=True)
+
+    model_y = LogisticRegression()
+    model_y.fit(x_train, y_train)
+
+    util.plot(x_test, y_test, model_y.theta, 'output/p02d.png')
+
+    y_pred = model_y.predict(x_test)
+    np.savetxt(pred_path_d, y_pred > 0.5, fmt='%d')
     # Make sure to save outputs to pred_path_d
     # Part (e): Apply correction factor using validation set and test on true labels
+    x_valid, y_valid = util.load_dataset(valid_path, label_col='y', add_intercept=True)
+
+    alpha = np.mean(model_y.predict(x_valid))
+
+    correction = 1 + np.log(2 / alpha - 1) / model_y.theta[0]
+    util.plot(x_test, t_test, model_y.theta, 'output/p02e.png', correction)
+
+    t_pred_e = y_pred / alpha
+    np.savetxt(pred_path_e, t_pred_e > 0.5, fmt='%d')
+
+    # correction =
     # Plot and use np.savetxt to save outputs to pred_path_e
     # *** END CODER HERE
--- PS1/src/p03d_poisson.py
+++ PS1/src/p03d_poisson.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import util
 
@@ -19,20 +20,24 @@ def main(lr, train_path, eval_path, pred_path):
     # x_train, y_train = util.load_dataset(train_path, add_intercept=False)
 
     # *** START CODE HERE ***
-    # Fit a Poisson Regression model
-    # Run on the validation set, and use np.savetxt to save outputs to pred_path
+
+    model = PoissonRegression(step_size=lr, eps=1e-5)
+    model.fit(x_train, y_train)
+
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    y_pred = model.predict(x_eval)
+    np.savetxt(pred_path, y_pred)
+
+    plt.figure()
+    plt.plot(y_eval, y_pred, 'bx')
+    plt.xlabel('true counts')
+    plt.ylabel('predict counts')
+    plt.savefig('output/p03d.png')
+
     # *** END CODE HERE ***
 
 
 class PoissonRegression(LinearModel):
-    """Poisson Regression.
-
-    Example usage:
-        > clf = PoissonRegression(step_size=lr)
-        > clf.fit(x_train, y_train)
-        > clf.predict(x_eval)
-    """
-
     def fit(self, x, y):
         """Run gradient ascent to maximize likelihood for Poisson regression.
 
@@ -41,6 +46,17 @@ class PoissonRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+
+        m, n = x.shape
+        self.theta = np.zeros(n)
+
+        while True:
+            theta = np.copy(self.theta)
+            self.theta += self.step_size * x.T.dot(y - np.exp(x.dot(self.theta))) / m
+
+            if np.linalg.norm(self.theta - theta, ord=1) < self.eps:
+                break
+
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -53,4 +69,7 @@ class PoissonRegression(LinearModel):
             Floating-point prediction for each input, shape (m,).
         """
         # *** START CODE HERE ***
+
+        return np.exp(x.dot(self.theta))
+
         # *** END CODE HERE ***
